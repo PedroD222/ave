801eabd0c57aebaed13005d64ed248da67a98a7a
PedroD222 <pedroduarte19@sapo.pt>
2015-01-08 22:02:17 +0000

    add proj

---
+++ DynamicProxyFactory/DynamicProxyFactory/DynamicProxyFactory.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProxy
+{
+    public class CallInfo {
+        private MethodInfo _targetMethod;
+        private object _target;
+        private object[] _parameters;
+
+        public CallInfo(MethodInfo tm, object t, object[] pm){
+            _targetMethod = tm;
+            _target = t;
+            _parameters = pm;
+        }
+        public MethodInfo TargetMethod {
+            get{ return _targetMethod; }
+        }
+
+        public object Target{
+            get{ return _target; }
+        }
+        public object[]Parameters{
+            get{ return _parameters; }
+        }
+    }
+
+    public interface IInvocationHandler{
+        object OnCall(CallInfo info);
+    }
+
+    class DynamicProxyFactory
+    {
+        static T MakeProxy<T>(Object oBase, IInvocationHandler handler)  {
+            AssemblyBuilder ab = AppDomain.CurrentDomain.DefineDynamicAssembly(null, AssemblyBuilderAccess.RunAndCollect); //Pode dar problemas
+            ModuleBuilder mb = ab.DefineDynamicModule(null);
+            TypeBuilder tb = mb.DefineType(null, TypeAttributes.Public, oBase.GetType());
+
+            foreach (ConstructorInfo cInfo in oBase.GetType().GetConstructors())
+            {
+                
+            }
+
+            foreach (MethodInfo mInfo in oBase.GetType().GetRuntimeMethods())
+            {
+                //generate method
+                //build CallInfo
+                //call handler.OnCall(CallInfo)
+            }
+
+            return default(T);
+        }
+    }
+}
+
+/*
+*class Proxy : Foo
+{
+    IInvocationHandler handler;
+    public Proxy (IInvocationHandler handler){
+        this.handler = handler;
+    }
+    public int DoIt(String s){
+        CallInfo ci = new CallInfo(typeof(Foo).GetMethod("DoIt"), this, new object[]{s});
+        return (int)handler.OnCall(ci);
+    }
+}
+*/
---
+++ DynamicProxyFactory/DynamicProxyFactory/app.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Program
+{
+	public class Foo {
+public virtual int DoIt(String v) {
+Console.WriteLine(
+"AClass.DoIt() with {0}",
+v
+);
+return v.Length;
+}
+}
+class LoggerInterceptor : IInvocationHandler {
+private long start;
+private Stopwatch watch = new Stopwatch();
+public object OnCall(CallInfo info) {
+start = watch.ElapsedTicks;
+// call real method using reflection
+object res = info.TargetMethod.Invoke(
+info.Target,
+info.Parameters);
+Console.WriteLine("Executed in {0} ticks",
+watch.ElapsedTicks - start);
+return res;
+}
+}
+IInvocationHandler logInterceptor =
+new LoggerInterceptor();
+Foo real = new Foo();
+Foo proxy =
+DynamicProxyFactory.MakeProxy<Foo>(
+real,
+logInterceptor
+);
+proxy.DoIt(12);
+Listagem 2: Criação de proxy para uma instância da classe Foo
+Na Listagem 3 é instanciado um proxy que implementa a interface IHelper e cujo comportamento é
+especificado pelo tipo MockInterceptor.
+interface IHelper {
+string Operation(
+IDictionary<int, string> param);
+}
+class MockInterceptor : IInvocationHandler
+{
+public object OnCall(CallInfo info)
+{
+// just a mock interceptor
+// that always returns the same value
+return “some text”;
+}
+}
+IInvocationHandler mockInterceptor =
+new MockInterceptor();
+IHelper p =
+DynamicProxyFactory.MakeProxy<IHelper>(
+mockInterceptor
+);
+string s = p.Operation(
+new Dictionary<int, string>());
+}
