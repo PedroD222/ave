6f6ff283660ef6e8e2e4d171b79b5963fafc585b
PedroD222 <pedroduarte19@sapo.pt>
2015-01-31 19:56:08 +0000

    add maintest

---
+++ DynamicProxyFactory/DynamicProxyManager/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProxyManager
+{
+    public class Foo
+    {
+        public virtual int DoIt(String v)
+        {
+            Console.WriteLine("AClass.DoIt() with {0}", v);
+            return v.Length;
+        }
+    }
+
+    public class App<T>
+    {
+        public static void Main(string[] args)
+        {
+            Foo f = new Foo();
+            f = DynamicProxyManager<string>.With<string>();
+            String ola = "bi";
+            String o = "w";
+            String [] ol = {o, ola};
+            
+            MethodInfo m ;
+        }
+    }
+}
