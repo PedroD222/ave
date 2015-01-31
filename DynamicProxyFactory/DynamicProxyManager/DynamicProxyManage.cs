321a1a11a5515d5dc062fc9ba0a97e977ea3d0ad
RubenSantos <rubensantos35@hotmail.com>
2015-01-31 14:48:12 +0000

    Inital stage of phase 2

---
+++ DynamicProxyFactory/DynamicProxyManager/DynamicProxyManage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DynamicProxyManager
+{
+    public class DynamicProxyManager<T>
+    {
+        private static Type type;
+        private static Delegate del;
+
+        public static T With<T>(this T src) where T : class
+        {
+            type = typeof(T);
+            return src;
+        }
+
+        public static T On<IEnumerable<U>>(this T src, Delegate mi) where T : class
+        {
+            del = mi;
+            return src;
+        }
+    }
+}
