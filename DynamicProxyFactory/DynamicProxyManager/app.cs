70285535c09f176f870b464ee7e49b5ae3a55bc0
PedroD222 <pedroduarte19@sapo.pt>
2015-02-02 21:36:15 +0000

    a t

--- DynamicProxyFactory/DynamicProxyManager/app.cs
+++ DynamicProxyFactory/DynamicProxyManager/app.cs
@@ -62,23 +62,45 @@ namespace DynamicProxyManager
                 .Make();
             int n = real.DoIt("cenas");
             Console.WriteLine("Proxy.Doit : "+ n );
-
+            Console.WriteLine();
+            Foo real2 = new Foo();
+            real2 = DynamicProxyFactory
+                .With<Foo>(real2)
+                .On<String, int>(real2.DoIt)
+                .DoAfter<String>(v => Console.WriteLine("AFTER1 {0}", v))
+                .On<String>(real2.ToString)
+                .DoBefore( () => Console.WriteLine("BEFORE real.toSt"))
+                .Make();
+            real2.DoIt("12");
+            real2.ToString();
+            Console.WriteLine();
             FooWithDoitVoidRet proxy = new FooWithDoitVoidRet();
-            proxy = DynamicProxyFactory.With<FooWithDoitVoidRet>(proxy).On<String>(proxy.DoIt).Make();
+            proxy = DynamicProxyFactory.With<FooWithDoitVoidRet>(proxy)
+                                        .On<String>(proxy.DoIt)
+                                        .DoAfter<String>(v => Console.WriteLine("AFTER1 {0}", v))
+                                        .DoBefore<String>(v => Console.WriteLine("BEFORE1 with {0}", v)).Make();
             Console.WriteLine(proxy.DoIt());
-
+            Console.WriteLine();
             FooWithDoitTwoP proxy2 = new FooWithDoitTwoP();
-            proxy2 = DynamicProxyFactory.With<FooWithDoitTwoP>(proxy2).On<String, String, int>(proxy2.DoIt).Make();
+            proxy2 = DynamicProxyFactory.With<FooWithDoitTwoP>(proxy2)
+                                        .On<String, String, int>(proxy2.DoIt)
+                                        .DoAfter<String>(v => Console.WriteLine("AFTER1 {0}", v))
+                                        .Make();
             proxy2.DoIt(s1, s2);
-
+            Console.WriteLine();
             FooWithDoitThreeP proxy3 = new FooWithDoitThreeP();
-            proxy3 = DynamicProxyFactory.With<FooWithDoitThreeP>(proxy3).On<String, String, String, int>(proxy3.DoIt).Make();
+            proxy3 = DynamicProxyFactory.With<FooWithDoitThreeP>(proxy3)
+                                        .On<String, String, String, int>(proxy3.DoIt)
+                                        .DoBefore<String>(v => Console.WriteLine("BEFORE1 with {0}", v)).Make();
             proxy3.DoIt(s1, s2, s3);
-
+            Console.WriteLine();
             FooVoid proxy4 = new FooVoid();
-            proxy4 = DynamicProxyFactory.With<FooVoid>(proxy4).On(proxy4.DoIt).Make();
+            proxy4 = DynamicProxyFactory.With<FooVoid>(proxy4)
+                                        .On(proxy4.DoIt)
+                                        .Replace(() => Console.Beep(20,200))
+                                        .Make();
             proxy4.DoIt();
-
+            Console.WriteLine();
             Console.ReadKey();
         }
     }
